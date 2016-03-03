47767c5e9e023cb4fe053171cab081fdb8635210
Mlekodaj <smlek001@int.usc.local>
2016-03-03 16:55:13 -0600

    more mlp

--- nnComparison.R
+++ nnComparison.R
@@ -1,16 +1,34 @@
 
 
 # standard backprop
+library(RSNNS)
 modelstd <- mlp(trainInputs, trainOutputs,
-                size=2,
+                size=5,
                 learnFunc="Std_Backpropagation", learnFuncParams=c(0.01, 0),
                 maxit=500,
                 linOut = FALSE,
                 inputsTest = cvInputs,
                 targetsTest = cvOutputs
 )
+modelstd <- mlp(trainInputs, trainOutputs,
+                size=5,
+                learnFunc="Quickprop", 
+                learnFuncParams=c(0.1, 2.0, 0.0001, 0.1),
+                maxit=500,
+                linOut = FALSE
+)
+modelstd <- mlp(trainInputs, trainOutputs,
+                size=5,
+                learnFunc="BackpropWeightDecay", 
+                learnFuncParams=c(0.2, .01, 0, 0),
+                maxit=500,
+                linOut = FALSE
+)
+detach(RSNNS)
 
 ## nnet vs neuralnet
+library(neuralnet)
+
 nn.bp <- neuralnet(fo, data = trainData,
                    hidden=2,
                    threshold = 0.01,
@@ -24,8 +42,43 @@ nn.bp <- neuralnet(fo, data = trainData,
 )
 nn.bp
 
-# also try: caret, 
+nn <- neuralnet(fo, data = trainData,
+                   hidden=2,
+                   threshold = 0.01,
+                   stepmax = 1e+5,
+                   learningrate = 0.01,
+                   lifesign = "full",
+                   algorithm = "rprop+",
+                   err.fct = "ce",
+                   act.fct = "logistic",
+                   linear.output = FALSE
+)
+nn
+detach(neuralnet)
 
 
-nn.nnet <- nnet(fo, data = trainData, size = 2, entropy = T, abstol = 0.01)
+library(nnet)
+nn.nnet <- nnet(fo, data = trainData, 
+                size = 5,
+                #entropy = T,
+                abstol = 0.01,
+                decay = 5e-4,
+                maxit = 5000,
+                linout = FALSE
+                )
+nn.nnet <- nnet(x = trainInputs,
+                #y = trainOutputs,
+                y = as.numeric(trainOutputs),
+                size = 5,
+                entropy = T,
+                abstol = 0.01,
+                decay = 5e-4,
+                maxit = 5000,
+                linout = FALSE
+)
+
+
+
 nn.nnet
+
+detach(nnet)
